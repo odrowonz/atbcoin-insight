321bfd1c31bf86879194807b9f296065bc02ef33
segwit <segwit@gmail.com>
2017-06-02 13:12:56 +0300

    tab od send transactions is complete

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -149,7 +149,6 @@ OverviewPage::OverviewPage(const PlatformStyle *platformStyle, QWidget *parent) 
     ui->listTransactions->setIconSize(QSize(DECORATION_SIZE, DECORATION_SIZE));
     ui->listTransactions->setMinimumHeight(NUM_ITEMS * (DECORATION_SIZE + 2));
     ui->listTransactions->setAttribute(Qt::WA_MacShowFocusRect, false);
-    ui->listTransactions->setToolTip("");
 
     connect(ui->listTransactions, SIGNAL(clicked(QModelIndex)), this, SLOT(handleTransactionClicked(QModelIndex)));
 
