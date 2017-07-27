85e8be27971731556a3986a916a6a2483ce06850
segwit <segwit@gmail.com>
2017-07-27 11:32:07 +0300

    Share Dialog is completed

--- src/qt/ShareDialog.cpp
+++ src/qt/ShareDialog.cpp
@@ -7,6 +7,7 @@
 #include "../pubkey.h"
 #include "../txmempool.h"
 #include "informationdialog.h"
+#include "../key.h"
 ShareDialog::ShareDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ShareDialog)
@@ -25,22 +26,51 @@ void ShareDialog::resizeEvent(QResizeEvent *){
 }
 void ShareDialog::sendClickedByKey(){
     ui->ResponceByKey->setVisible(true);
-    std::string key= ui->KeyEdit->text().toStdString();
-    key.erase(std::remove(key.begin(), key.end(), ' '), key.end());
-    CPubKey pubkey;
-    pwalletMain->GetKeyFromPool(pubkey);
-    if(!pwalletMain->AddCryptedKey(pubkey,std::vector<unsigned char>(key.begin(),key.end()))){
-        ui->ResponceByKey->setText(tr("This key is not valid."));
+    std::string key_text= ui->KeyEdit->text().toStdString();
+    key_text.erase(std::remove(key_text.begin(), key_text.end(), ' '), key_text.end());
+    CBitcoinSecret vchSecret;
+    bool fGood = vchSecret.SetString(key_text);
+    ui->ResponceByKey->setStyleSheet("QLabel { color: #f6e395; }");
+    if (!fGood){
+        ui->ResponceByKey->setText(tr("Invalid private key encoding."));
         return;
     }
-    CWallet* wallet =pwalletMain;
-    CAmount ammout=wallet->GetBalance();
-    pwalletMain->ScanForWalletTransactions(chainActive.Genesis(), true);
-    pwalletMain->ReacceptWalletTransactions();
-    if(ammout>=pwalletMain->GetBalance()){
-        ui->ResponceByKey->setText(tr("Could not get share by key (%0)").arg(QString::fromStdString(key)));
+
+    CKey key = vchSecret.GetKey();
+    if (!key.IsValid()){
+        ui->ResponceByKey->setText(tr("Private key outside allowed range."));
         return;
     }
+    CPubKey pubkey = key.GetPubKey();
+//    std::vector<unsigned char> data(pubkey.g.begin(), pubkey.end());
+//    std::stringstream hex;
+//    for(std::vector<unsigned char>::iterator i=data.begin(); i!=data.end(); i++){
+//        hex << std::hex << (int)*i;
+//    }
+    InformationDialog(QString::fromStdString(pubkey.GetHash().ToString()),"","",this).exec();
+    CKeyID vchAddress = pubkey.GetID();
+    {
+        pwalletMain->MarkDirty();
+        pwalletMain->SetAddressBook(vchAddress, key_text, "receive");
+        if (pwalletMain->HaveKey(vchAddress)){
+            ui->ResponceByKey->setText(tr("A private key has already been added to your wallet."));
+            return;
+        }
+        pwalletMain->mapKeyMetadata[vchAddress].nCreateTime = 1;
+
+        if (!pwalletMain->AddKeyPubKey(key, pubkey)){
+            ui->ResponceByKey->setText(tr("Error adding key to wallet."));
+            return;
+        }
+        pwalletMain->nTimeFirstKey = 1; // 0 would be considered 'no value'
+        CAmount ammout=pwalletMain->GetBalance();
+        pwalletMain->ScanForWalletTransactions(chainActive.Genesis(), true);
+        if(ammout>=(pwalletMain->GetBalance()+pwalletMain->GetImmatureBalance()+pwalletMain->GetUnconfirmedBalance())){
+            ui->ResponceByKey->setText(tr("Could not get share by key (%0)").arg(QString::fromStdString(key_text)));
+            return;
+        }
+    }
+    ui->ResponceByKey->setStyleSheet("QLabel { color: #a3f642; }");
     ui->ResponceByKey->setText(tr("Your share is added to the wallet."));
 }
 void ShareDialog::sendClickedByEmail(){
